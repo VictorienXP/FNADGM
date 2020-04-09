1eb555010636775c6c3a299cea3b522ac30694e5
VictorienXP <VictorienXP@Xperidia.com>
2020-04-09 12:18:23 +0200

    Update cl_monitor_shared.lua

--- gamemodes/fnafgm/gamemode/cl_monitor_shared.lua
+++ gamemodes/fnafgm/gamemode/cl_monitor_shared.lua
@@ -1,7 +1,7 @@
 --[[---------------------------------------------------------
 
 	Five Nights at Freddy's Gamemode for Garry's Mod
-			by VictorienXP@Xperidia (2015)
+			by VictorienXP@Xperidia (2015-2020)
 
 	"Five Nights at Freddy's" is a game by Scott Cawthon.
 
