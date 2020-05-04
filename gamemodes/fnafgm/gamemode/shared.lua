739da2fd3bc918efd6b6247e086ec389b9aa3040
VictorienXP <VictorienXP@Xperidia.com>
2020-05-04 15:01:28 +0200

    Update shared.lua

--- gamemodes/fnafgm/gamemode/shared.lua
+++ gamemodes/fnafgm/gamemode/shared.lua
@@ -15,7 +15,7 @@ GM.Name 			= "Five Nights at Freddy's"
 GM.ShortName 		= "FNAFGM"
 GM.Author 			= "VictorienXP@Xperidia"
 GM.Website 			= "steamcommunity.com/sharedfiles/filedetails/?id=408243366"
-GM.OfficialVersion 	= "SuperDean-ae90bd3"
+GM.OfficialVersion 	= "SuperDean-b8ea55a"
 GM.Version 			= GM.OfficialVersion
 GM.Official			= false
 GM.IsFNAFGMDerived	= true
